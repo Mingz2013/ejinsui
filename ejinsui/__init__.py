117b6b1a217f80b76dc9371d58616553bc047f54
mingz <305603665@qq.com>
2016-07-18 14:58:23 +0800

    init

--- ejinsui/__init__.py
+++ ejinsui/__init__.py
@@ -6,6 +6,7 @@ from mongo import CompanyDB
 from read_json import read_json
 
 def run(rootdir):
+    i = 0
     for parent, dirnames, filenames in os.walk(rootdir):
         for filename in filenames:
             # print rootdir, filename
@@ -13,7 +14,8 @@ def run(rootdir):
                 json_list = read_json(rootdir + '/' + filename)
                 for json_obj in json_list:
                     CompanyDB.upsert_company(json_obj)
-                    print json_obj
+                    i += 1
+                    print i, json_obj
                     pass
             except Exception, e:
                 print e.message
